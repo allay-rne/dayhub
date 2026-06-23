08134a8b13258ea2cd337c5b71b5b73f5cd837d5
Ilya Ilyin <allayrnw@gmail.com>
2026-06-23 12:21:08 +0700

    feat(home): add HomeCard component

--- src/shared/ui/HomeCard/HomeCard.jsx
+++ src/shared/ui/HomeCard/HomeCard.jsx
@@ -1,16 +1,34 @@
 import './HomeCard.scss'
-import classNames from 'classnames'
+import Icon from "@/shared/ui/Icon";
 
 const HomeCard = (props) => {
   const {
-    className,
+    name,
+    title,
+    value,
+    subtitle,
+    children,
   } = props
 
   return (
-    <div
-      className={classNames(className, 'home-card')}
-    >
-      HomeCard
+    <div className="home-card">
+      <div className="home-card__header">
+        <Icon name={name} />
+        <h3 className="home-card__header-title">{title}</h3>
+      </div>
+
+      <div className="home-card__main">
+        <p className="home-card__main-value">{value}</p>
+        <p className="home-card__subtitle">{subtitle}</p>
+      </div>
+
+      {children && <div className="home-card__footer">
+        {children}
+      </div>
+      }
+
     </div>
   )
 }
+
+export default HomeCard
