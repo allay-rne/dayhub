c565d81ce4027cc8012fb8727bb8234de8a39c27
Ilya Ilyin <allayrnw@gmail.com>
2026-06-26 18:55:29 +0700

    fix(notes): exclude deleted notes from tags count

--- src/entities/notes/model/useNoteSide.js
+++ src/entities/notes/model/useNoteSide.js
@@ -2,7 +2,7 @@ const useNotesSide = (notes) => {
 
   const allNotes = notes.filter((note) => !note.isDeleted).length
   const favoriteNotes = notes.filter((note) => note.isFavorite && !note.isDeleted === true).length
-  const tagsNote = notes.filter((note) => note.tags?.length > 0).length
+  const tagsNote = notes.filter((note) => note.tags?.length > 0 && !note.isDeleted).length
   const trashNote = notes.filter((note) => note?.isDeleted).length
 
   return {
